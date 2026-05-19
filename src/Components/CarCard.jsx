0674d7caf1334bdc778630c5e3a49f6c60060a3d
Simanto Poddar <251956045+SIMANTO-PODDAR@users.noreply.github.com>
2026-05-19 12:15:06 +0600

    complete better auth setup

--- src/Components/CarCard.jsx
+++ src/Components/CarCard.jsx
@@ -10,24 +10,26 @@ const CarCard = ({ car }) => {
                         src={car.ImgURL}
                         alt={car.Name} />
                 </figure>
-                <div className="card-body">
-                    <h2 className="card-title">
-                        <p className="text-sm text-left font-bold text-[#0D0D33]"> {car.Name}</p>
+                <div className="flex mt-auto">
+                    <div className="card-body h-[165px]">
+                        <h2 className="card-title">
+                            <p className="text-sm text-left font-bold text-[#0D0D33]"> {car.Name}</p>
 
-                        <div className={`badge badge-outline  badge-sm ${car.Status == "Available" ? 'badge-success' : 'badge-warning'}`}>{car.Status}</div>
+                            <div className={`badge badge-outline  badge-sm ${car.Status == "Available" ? 'badge-success' : 'badge-warning'}`}>{car.Status}</div>
 
-                    </h2>
-                    <p className="text-justify">
-                        {car.Description}
-                    </p>
-                    <div className="card-actions justify-between">
-                        <div className="badge badge-outline">
-                            $ {car.RentPrice}
+                        </h2>
+                        <p className="text-justify line-clamp-2">
+                            {car.Description}
+                        </p>
+                        <div className="card-actions justify-between items-baseline">
+                            <div className="badge badge-outline">
+                                $ {car.RentPrice}
+                            </div>
+
+                            <Link href={`/explore-cars/${car._id}`} >
+                                <Button className="btn btn-sm flex gap-2  items-center justify-start font-bold text-white bg-linear-to-r from-[#0D0D33] to-[#0033FF]">Details</Button>
+                            </Link>
                         </div>
-
-                        <Link href={`/explore-cars/${car._id}`} >
-                            <Button className="btn btn-sm flex gap-2  items-center justify-start font-bold text-white bg-linear-to-r from-[#0D0D33] to-[#0033FF]">Details</Button>
-                        </Link>
                     </div>
                 </div>
             </div>
